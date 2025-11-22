28d7c79b88c2eec1bbeeedabde1749ed181f3b60
Son Juwan <wndhks171@naver.com>
2025-11-22 14:27:27 +0900

    init/#1: yubin setting

---
+++ app/src/main/java/org/sopt/soptackthon_app_3/presentation/yubin/YubinScreen.kt
@@ -0,0 +1,50 @@
+package org.sopt.soptackthon_app_3.presentation.yubin
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.soptackthon_app_3.core.designsystem.theme.SopkathonTheme
+import org.sopt.soptackthon_app_3.core.util.noRippleClickable
+
+
+@Composable
+fun YubinRoute(
+    navigateToJuwan: () -> Unit,
+) {
+    YubinScreen(
+        navigateToJuwan = navigateToJuwan
+    )
+}
+
+@Composable
+fun YubinScreen(
+    modifier: Modifier = Modifier,
+    navigateToJuwan: () -> Unit = {},
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(
+                color = SopkathonTheme.colors.White
+            )
+    ) {
+        Text(
+            text = "Yubin Screen",
+            modifier = Modifier
+                .noRippleClickable(onClick = navigateToJuwan),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ReviewYubinScreen() {
+    SopkathonTheme {
+        YubinScreen()
+    }
+
+}
---
+++ app/src/main/java/org/sopt/soptackthon_app_3/presentation/yubin/viewmodel/YubinViewModel.kt
@@ -0,0 +1,6 @@
+package org.sopt.soptackthon_app_3.presentation.yubin.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class YubinViewModel : ViewModel() {
+}
