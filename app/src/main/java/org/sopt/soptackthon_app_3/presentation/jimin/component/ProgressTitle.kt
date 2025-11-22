b54e552987f218b4ed8e35adce6dd87829f778cf
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2025-11-23 00:18:02 +0900

    [feat/#4] ProgressTitle 추가

---
+++ app/src/main/java/org/sopt/soptackthon_app_3/presentation/jimin/component/ProgressTitle.kt
@@ -0,0 +1,47 @@
+package org.sopt.soptackthon_app_3.presentation.jimin.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.soptackthon_app_3.core.designsystem.theme.SopkathonTheme
+
+@Composable
+fun ProgressTitle(
+    title: String,
+    subTitle: String,
+    modifier: Modifier = Modifier
+) {
+    Column (
+        modifier = modifier
+    ){
+        Text(
+            text = title,
+            color = SopkathonTheme.colors.black,
+            style = SopkathonTheme.typography.title.titleSb16
+        )
+
+        Spacer(modifier = Modifier.height(2.dp))
+
+        Text(
+            text = subTitle,
+            color = SopkathonTheme.colors.black,
+            style = SopkathonTheme.typography.body.bodyM14
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProgressTitlePreview() {
+    ProgressTitle(
+        title = "Service progress",
+        subTitle = "Your reservation is confirmed!\n" +
+                "We’ll keep you updated as things progress."
+    )
+}
