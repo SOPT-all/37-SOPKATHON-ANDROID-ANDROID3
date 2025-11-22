a1427b00627875454d91d365be625d2172271b9d
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2025-11-23 01:42:35 +0900

    [feat/#12] progress screen 1차 추가

--- app/src/main/java/org/sopt/soptackthon_app_3/presentation/jimin/JiminScreen.kt
+++ app/src/main/java/org/sopt/soptackthon_app_3/presentation/jimin/JiminScreen.kt
@@ -1,14 +1,16 @@
 package org.sopt.soptackthon_app_3.presentation.jimin
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.sopt.soptackthon_app_3.core.designsystem.theme.SopkathonTheme
-import org.sopt.soptackthon_app_3.core.util.noRippleClickable
+import org.sopt.soptackthon_app_3.presentation.jimin.component.ProgressContentTitle
+import org.sopt.soptackthon_app_3.presentation.jimin.component.ProgressTitle
 
 
 @Composable
@@ -24,16 +26,31 @@ fun JiminScreen (
     modifier : Modifier = Modifier,
     navigateToYubin:() -> Unit = {}
 ){
-    Box(
-        modifier = Modifier
+    Column(
+        modifier = modifier
             .fillMaxSize()
             .background(color = SopkathonTheme.colors.white)
     ){
-        Text(
-            text = "Jimin Screen",
+        ProgressTitle(
+            title = "Service progress",
+            subTitle = "Your reservation is confirmed!\n" +
+                    "We’ll keep you updated as things progress.",
             modifier = Modifier
-                .noRippleClickable(onClick = navigateToYubin)
+                    .background(
+                color = SopkathonTheme.colors.primary500
+            )
         )
+
+        Column (
+            modifier = Modifier.padding(horizontal = 16.dp)
+        ){
+
+            ProgressContentTitle(
+                title = "Booking Information",
+                modifier = Modifier.padding(top = 32.dp, bottom = 16.dp)
+            )
+        }
+
     }
 
 }
