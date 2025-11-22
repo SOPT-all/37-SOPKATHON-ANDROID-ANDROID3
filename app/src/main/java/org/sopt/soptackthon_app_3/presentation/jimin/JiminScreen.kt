4c47f8e439eea710ce496556518f1a33c0bab81a
Son Juwan <wndhks171@naver.com>
2025-11-22 14:27:08 +0900

    init/#1: jimin setting

---
+++ app/src/main/java/org/sopt/soptackthon_app_3/presentation/jimin/JiminScreen.kt
@@ -0,0 +1,49 @@
+package org.sopt.soptackthon_app_3.presentation.jimin
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.soptackthon_app_3.core.designsystem.theme.SopkathonTheme
+import org.sopt.soptackthon_app_3.core.util.noRippleClickable
+
+
+@Composable
+fun JiminRoute(
+    navigateToYubin: () -> Unit
+){
+    JiminScreen(
+        navigateToYubin = navigateToYubin
+    )
+}
+@Composable
+fun JiminScreen (
+    modifier : Modifier = Modifier,
+    navigateToYubin:() -> Unit = {}
+){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = SopkathonTheme.colors.White
+            )
+    ){
+        Text(
+            text = "Jimin Screen",
+            modifier = Modifier
+                .noRippleClickable(onClick = navigateToYubin)
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ReviewJiminScreen(){
+    SopkathonTheme {
+        JiminScreen()
+    }
+}
---
+++ app/src/main/java/org/sopt/soptackthon_app_3/presentation/jimin/viewmodel/JiminViewModel.kt
@@ -0,0 +1,6 @@
+package org.sopt.soptackthon_app_3.presentation.jimin.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class JiminViewModel : ViewModel() {
+}
