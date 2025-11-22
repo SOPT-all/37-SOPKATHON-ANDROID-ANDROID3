8d6b7a799a10ccca94daad722987e6d97971c529
oilbeaneda <20221444@sungshin.ac.kr>
2025-11-22 23:19:39 +0900

    init/#2:디자인시스템 정의

--- app/src/main/java/org/sopt/soptackthon_app_3/core/designsystem/theme/Color.kt
+++ app/src/main/java/org/sopt/soptackthon_app_3/core/designsystem/theme/Color.kt
@@ -4,22 +4,61 @@ import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Black = Color(0xFF000000)
-val White = Color(0xFFFFFFFF)
 
-// 여기에 Color 코드
+//primary Color
+val primary_=Color(0xFF428FFF)
+
+val primary_900=Color(0xFF101827)
+
+val primary_800=Color(0xFF4B5563)
+
+val primary_500=Color(0xFF9CA2AE)
+
+val primary_300=Color(0xFFE5E7EA)
+
+val primary_200=Color(0XFFF2F4F5)
+
+val primary_100=Color(0XFFF8FAFB)
+
+
+//secondary Color
+
+val secondary_=Color(0xFFC8DCFE)
+
+
+//black_white
+val black_=Color(0xFF121212)
+val white_=Color(0xFFFFFFFF)
+
+
+
 
 @Immutable
 data class SopkathonColor(
-    val Black : Color,
-    val White : Color
-    // 요로코롬
-)
+
+    val primary:Color=primary_,
+    val primary900: Color=primary_900,
+    val primary800: Color=primary_800,
+    val primary500: Color= primary_500,
+    val primary300: Color= primary_300,
+    val primary200: Color=primary_200,
+    val primary100: Color=primary_100,
+    val black : Color=black_,
+    val white : Color=white_
+
+    )
 
 val defaultSopkathonColor = SopkathonColor(
-    Black = Black,
-    White = White
-    // 요런식으로
+
+    primary_,
+    primary900 = primary_900,
+    primary800 = primary_800,
+    primary500 = primary_500,
+    primary300 = primary_300,
+    primary200 = primary_200,
+    primary_100,
+    black = black_,
+    white = white_
 )
 
 
--- app/src/main/java/org/sopt/soptackthon_app_3/core/designsystem/theme/Type.kt
+++ app/src/main/java/org/sopt/soptackthon_app_3/core/designsystem/theme/Type.kt
@@ -5,31 +5,152 @@ import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import org.sopt.soptackthon_app_3.R
 
-val SopkathonRegularFont = FontFamily(Font(R.font.pretendard_regular))
+
+object SopkathonRegularFont {
+    val Bold = FontFamily(Font(R.font.roboto_bold))
+    val Regular = FontFamily(Font(R.font.pretendard_regular))
+    val SemiBold = FontFamily(Font(R.font.roboto_semibold))
+    val Medium = FontFamily(Font(R.font.roboto_medium))
+
+}
+
+private fun SopkathonTextStyle(
+    fontFamily: FontFamily,
+    fontSize: TextUnit,
+    lineHeight: TextUnit = 1.5.em,
+    letterSpacing: TextUnit
+): TextStyle = TextStyle(
+    fontFamily = fontFamily,
+    fontSize = fontSize,
+    lineHeight = lineHeight,
+    letterSpacing = letterSpacing,
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None
+    )
+)
+
+
+sealed interface TypographyTokens {
+    @Immutable
+    data class Heading(
+        val headingSb20: TextStyle,
+        val headingSb18: TextStyle,
+    )
+
+    @Immutable
+    data class Title(
+        val titleB16: TextStyle,
+        val titleB14: TextStyle,
+        val titleSb16: TextStyle,
+        val titleSb14: TextStyle,
+        val titleSb12: TextStyle,
+    )
+
+    @Immutable
+    data class Body(
+        val bodyM16: TextStyle,
+        val bodyM14: TextStyle,
+        val bodyM13: TextStyle
+    )
+
+    @Immutable
+    data class Caption(
+        val captionM12: TextStyle,
+        val captionR12: TextStyle
+    )
+
+
+}
 
 @Immutable
 data class SopkathonTypography(
-    val title: TextStyle,
-    // 여기에 추가 해주시면 돼용
+
+    val heading: TypographyTokens.Heading,
+    val title: TypographyTokens.Title,
+    val body: TypographyTokens.Body,
+    val caption: TypographyTokens.Caption
 )
 
 val defaultSopkathonTypography = SopkathonTypography(
-    title = TextStyle(
-        fontFamily = SopkathonRegularFont,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+
+    heading = TypographyTokens.Heading(
+        headingSb20 = SopkathonTextStyle(
+            fontFamily = SopkathonRegularFont.SemiBold,
+            fontSize = 16.sp,
+            letterSpacing = 0.sp
+        ),
+        headingSb18 = SopkathonTextStyle(
+            fontFamily = SopkathonRegularFont.SemiBold,
+            fontSize = 18.sp,
+            letterSpacing = 0.sp
+        )
+    ),
+    title = TypographyTokens.Title(
+        titleB16 = SopkathonTextStyle(
+            SopkathonRegularFont.Bold,
+            fontSize = 16.sp,
+            letterSpacing = 0.sp
+        ),
+        titleB14 = SopkathonTextStyle(
+            SopkathonRegularFont.Bold,
+            fontSize = 14.sp,
+            letterSpacing = (-1).sp
+        ),
+        titleSb16 = SopkathonTextStyle(
+            SopkathonRegularFont.SemiBold,
+            fontSize = 16.sp,
+            letterSpacing = (-1).sp
+        ),
+        titleSb14 = SopkathonTextStyle(
+            SopkathonRegularFont.SemiBold,
+            fontSize = 14.sp,
+            letterSpacing = (-1).sp
+        ),
+        titleSb12 = SopkathonTextStyle(
+            SopkathonRegularFont.SemiBold,
+            fontSize = 12.sp,
+            letterSpacing = (-1).sp
+        )
+    ),
+
+    body = TypographyTokens.Body(
+        bodyM16 = SopkathonTextStyle(
+            fontFamily = SopkathonRegularFont.Medium,
+            fontSize = 16.sp,
+            letterSpacing = (-2).sp
+        ),
+        bodyM14 = SopkathonTextStyle(
+            fontFamily = SopkathonRegularFont.Medium,
+            fontSize = 14.sp,
+            letterSpacing = (-2).sp
+        ),
+        bodyM13 = SopkathonTextStyle(
+            fontFamily = SopkathonRegularFont.Medium,
+            fontSize = 13.sp,
+            letterSpacing = (-2).sp
+        )
+    ),
+    caption = TypographyTokens.Caption(
+        captionM12 = SopkathonTextStyle(
+            fontFamily = SopkathonRegularFont.Medium,
+            fontSize = 12.sp,
+            letterSpacing = (-2).sp
+        ),
+        SopkathonTextStyle(
+            fontFamily = SopkathonRegularFont.Regular,
+            fontSize = 12.sp,
+            letterSpacing = (-2).sp
+        ),
     )
-    // 여기에 추가
-//    title_24Strong = TextStyle(
-//    fontFamily = HsLinkSemiBoldFont,
-//    fontWeight = FontWeight.SemiBold,
-//    fontSize = 24.sp,
-//    lineHeight = 34.sp,
-//    letterSpacing = (-0.03).em
+
+
 )
 
 val LocalSopkathonTypography = staticCompositionLocalOf { defaultSopkathonTypography }
