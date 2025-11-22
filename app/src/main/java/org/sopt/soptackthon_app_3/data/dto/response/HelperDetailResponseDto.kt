2f619953a7d3c4647a198e2cc8990195d6edb3e3
dodo <eg4rang@naver.com>
2025-11-23 05:47:02 +0900

    feat/#24: 헬퍼 디테일 API 작업

---
+++ app/src/main/java/org/sopt/soptackthon_app_3/data/dto/response/HelperDetailResponseDto.kt
@@ -0,0 +1,52 @@
+package org.sopt.soptackthon_app_3.data.dto.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class HelperDetailResponseDto(
+    @SerialName("helperId")
+    val helperId: Long,
+    @SerialName("imageURL")
+    val imageUrl: String,
+    @SerialName("name")
+    val name: String,
+    @SerialName("location")
+    val location: String,
+    @SerialName("rate")
+    val rate: Float,
+    @SerialName("verified")
+    val verified: Boolean,
+    @SerialName("serviceOffers")
+    val serviceOffers: List<ServiceOfferDto>,
+    @SerialName("about")
+    val about: String,
+    @SerialName("experience")
+    val experience: String,
+    @SerialName("availabilities")
+    val availabilities: List<AvailabilityDto>,
+    @SerialName("languages")
+    val languages: List<String>,
+    @SerialName("hourlyRate")
+    val hourlyRate: String
+)
+
+@Serializable
+data class ServiceOfferDto(
+    @SerialName("serviceOfferId")
+    val serviceOfferId: Long,
+    @SerialName("name")
+    val name: String,
+    @SerialName("takenTime")
+    val takenTime: Int,
+    @SerialName("serviceFee")
+    val serviceFee: String
+)
+
+@Serializable
+data class AvailabilityDto(
+    @SerialName("availableDayOfTheWeek")
+    val availableDayOfTheWeek: String,
+    @SerialName("availableTime")
+    val availableTime: String
+)
