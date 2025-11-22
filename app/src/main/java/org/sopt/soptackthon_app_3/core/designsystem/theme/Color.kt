c8763dfdd6f6ce987a875b65495e4533b4f0c668
Son Juwan <wndhks171@naver.com>
2025-11-23 03:32:20 +0900

    feat/#14: color fix

--- app/src/main/java/org/sopt/soptackthon_app_3/core/designsystem/theme/Color.kt
+++ app/src/main/java/org/sopt/soptackthon_app_3/core/designsystem/theme/Color.kt
@@ -6,46 +6,47 @@ import androidx.compose.ui.graphics.Color
 
 
 //primary Color
-val primary_=Color(0xFF428FFF)
+val primary_ = Color(0xFF428FFF)
 
-val primary_900=Color(0xFF101827)
+val primary_900 = Color(0xFF101827)
 
-val primary_800=Color(0xFF4B5563)
+val primary_800 = Color(0xFF4B5563)
 
-val primary_500=Color(0xFF9CA2AE)
+val primary_500 = Color(0xFF9CA2AE)
 
-val primary_300=Color(0xFFE5E7EA)
+val primary_400 = Color(0xFFD3D5D8)
 
-val primary_200=Color(0XFFF2F4F5)
+val primary_300 = Color(0xFFE5E7EA)
 
-val primary_100=Color(0XFFFDFFFF)
+val primary_200 = Color(0XFFF2F4F5)
+
+val primary_100 = Color(0XFFFDFFFF)
 
 
 //secondary Color
 
-val secondary_=Color(0xFFC8DCFE)
+val secondary_ = Color(0xFFC8DCFE)
 
 
 //black_white
-val black_=Color(0xFF121212)
-val white_=Color(0xFFFFFFFF)
-
-
+val black_ = Color(0xFF121212)
+val white_ = Color(0xFFFFFFFF)
 
 
 @Immutable
 data class SopkathonColor(
 
-    val primary:Color=primary_,
-    val secondary:Color = secondary_,
-    val primary900: Color=primary_900,
-    val primary800: Color=primary_800,
-    val primary500: Color= primary_500,
-    val primary300: Color= primary_300,
-    val primary200: Color=primary_200,
-    val primary100: Color=primary_100,
-    val black : Color=black_,
-    val white : Color=white_
+    val primary: Color = primary_,
+    val secondary: Color = secondary_,
+    val primary900: Color = primary_900,
+    val primary800: Color = primary_800,
+    val primary500: Color = primary_500,
+    val primary400: Color = primary_400,
+    val primary300: Color = primary_300,
+    val primary200: Color = primary_200,
+    val primary100: Color = primary_100,
+    val black: Color = black_,
+    val white: Color = white_,
 
     )
 
@@ -56,6 +57,7 @@ val defaultSopkathonColor = SopkathonColor(
     primary900 = primary_900,
     primary800 = primary_800,
     primary500 = primary_500,
+    primary400 = primary_400,
     primary300 = primary_300,
     primary200 = primary_200,
     primary_100,
