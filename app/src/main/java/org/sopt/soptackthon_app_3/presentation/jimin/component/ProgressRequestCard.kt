a600e6e82285aceb51d8f9a302658f07b77d39d6
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2025-11-23 01:02:54 +0900

    [feat/#4] ProgressCard 통합
    - 리퀘스트 카드
    - 프리 리포트 카드

--- app/src/main/java/org/sopt/soptackthon_app_3/presentation/jimin/component/ProgressRequestCard.kt
+++ app/src/main/java/org/sopt/soptackthon_app_3/presentation/jimin/component/ProgressRequestCard.kt
@@ -2,30 +2,37 @@ package org.sopt.soptackthon_app_3.presentation.jimin.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import org.sopt.soptackthon_app_3.core.designsystem.theme.SopkathonTheme
 
 @Composable
-fun ProgressRequestCard(
+fun ProgressCard(
     content: String,
+    backgroundColor: Color,
+    horizontalPadding: Dp,
     modifier: Modifier = Modifier
 ) {
     Box(
         modifier = modifier
             .fillMaxWidth()
             .background(
-                color = SopkathonTheme.colors.primary200,
+                color = backgroundColor,
                 shape = RoundedCornerShape(16.dp)
             )
-            .padding(horizontal = 18.dp, vertical = 20.dp),
+            .padding(horizontal = horizontalPadding, vertical = 20.dp),
         contentAlignment = Alignment.Center
     ) {
         Text(
@@ -38,8 +45,29 @@ fun ProgressRequestCard(
 
 @Preview(showBackground = true)
 @Composable
-private fun ProgressRequestCardPreview() {
-    ProgressRequestCard(
-        content = "Please pay special attention to my parents and make sure they’re well taken care of."
-    )
+private fun ProgressCardPreview() {
+    Column {
+        ProgressCard(
+            content = "Please pay special attention to my parents and make sure they’re well taken care of.",
+            backgroundColor = SopkathonTheme.colors.primary200,
+            horizontalPadding = 18.dp
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        ProgressCard(
+            content = "Hello, this is Sarah Johnson.\n" +
+                    "\n" +
+                    "I will be visiting today at 23:10 to begin the Grocery Shopping service.\n" +
+                    "\n" +
+                    "What I'm bringing:\n" +
+                    "- Cleaning supplies\n" +
+                    "- Shopping bags\n" +
+                    "- Face mask\n" +
+                    "\n" +
+                    "Please feel free to relax while I handle everything. Looking forward to helping!",
+            backgroundColor = SopkathonTheme.colors.primary300,
+            horizontalPadding = 23.dp
+        )
+    }
 }
