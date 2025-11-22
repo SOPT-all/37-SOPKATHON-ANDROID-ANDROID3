4315786fcd6c55165a03eaaee9e249456a9ed615
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2025-11-23 07:00:27 +0900

    mod/#12 하단 버튼 추가

--- app/src/main/java/org/sopt/soptackthon_app_3/presentation/jimin/JiminScreen.kt
+++ app/src/main/java/org/sopt/soptackthon_app_3/presentation/jimin/JiminScreen.kt
@@ -1,27 +1,20 @@
 package org.sopt.soptackthon_app_3.presentation.jimin
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -33,8 +26,6 @@ import org.sopt.soptackthon_app_3.core.designsystem.theme.SopkathonTheme
 import org.sopt.soptackthon_app_3.presentation.jimin.component.AnimateReportCard
 import org.sopt.soptackthon_app_3.presentation.jimin.component.BookingInfoCard
 import org.sopt.soptackthon_app_3.presentation.jimin.component.LinearProgressBar
-import org.sopt.soptackthon_app_3.presentation.jimin.component.ProgressBorderCard
-import org.sopt.soptackthon_app_3.presentation.jimin.component.ProgressCard
 import org.sopt.soptackthon_app_3.presentation.jimin.component.ProgressContentTitle
 import org.sopt.soptackthon_app_3.presentation.jimin.component.ProgressTitle
 import org.sopt.soptackthon_app_3.presentation.jimin.viewmodel.JiminViewModel
@@ -83,92 +74,103 @@ fun JiminScreen (
     modifier : Modifier = Modifier,
     navigateToYubin:() -> Unit = {}
 ){
-
-    Column(
-        modifier = modifier
+    Box(
+        modifier = Modifier
             .fillMaxSize()
-            .background(color = SopkathonTheme.colors.white)
-    ){
-        SopkathonTopbar()
+            .navigationBarsPadding()
+    )
 
-        ProgressTitle(
-            title = "Service progress",
-            subTitle = "Your reservation is confirmed!\n" +
-                    "You can check the progress of your request here.\n" +
-                    "We’ll safely follow up with your parents."
-        )
+    {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(color = SopkathonTheme.colors.white)
+        ) {
+            SopkathonTopbar(modifier = Modifier.systemBarsPadding())
 
-        Spacer(modifier = Modifier.height(32.dp))
-
-        LinearProgressBar()
-
-        Spacer(modifier = Modifier.height(32.dp))
-
-        Column (
-            modifier = Modifier
-                .background(color = SopkathonTheme.colors.primary200)
-                .padding(horizontal = 16.dp)
-                .verticalScroll(rememberScrollState())
-        ){
-
-            ProgressContentTitle(
-                title = "Booking Info",
-                modifier = Modifier.padding(top = 32.dp, bottom = 8.dp)
-            )
-
-            BookingInfoCard(
-                locateText = locateText,
-                dateText = dateText,
-                startTime = startTime,
-                confirmTime = confirmText,
-                serviceFee = serviceFee,
-                bookingFee = bookingFee,
-                cashText = cashText,
-                bookingInfoModel = BookingInfoModel(
-                    reportContent = bookingInfoModel.reportContent,
-                    reportBackgroundColor = SopkathonTheme.colors.primary200,
-                    reportCardHorizontalPadding = bookingInfoModel.reportCardHorizontalPadding
-                )
+            ProgressTitle(
+                title = "Service progress",
+                subTitle = "Your reservation is confirmed!\n" +
+                        "You can check the progress of your request here.\n" +
+                        "We’ll safely follow up with your parents."
             )
 
             Spacer(modifier = Modifier.height(32.dp))
 
-            AnimateReportCard(
-                title = "Pre-Service Report",
-                imgRes = R.drawable.pre_img,
-                content = "Hello, this is Sarah Johnson.\n" +
-                        "I’ll be arriving today at 11:10 PM to begin the Grocery Shopping service.\n" +
-                        "What I’ll bring:\n" +
-                        "- Cleaning supplies\n" +
-                        "- Shopping bags\n" +
-                        "- Face mask\n" +
-                        "Please relax while I take care of everything for you. I’m looking forward to helping!",
-                isVisible = isPreAnimateVisible
-            )
+            LinearProgressBar()
 
-            Spacer(modifier = Modifier.height(24.dp))
+            Spacer(modifier = Modifier.height(32.dp))
 
-            AnimateReportCard(
-                title = "Post-Service Report",
-                imgRes = R.drawable.new_img,
-                content = "Hello, this is Sarah Johnson.\n I’ve just finished the Grocery Shopping service for your parent.\n" +
-                        "Here’s what I completed today:\n" +
-                        "Purchased all items on the list\n" +
-                        "Organized everything neatly at home\n" +
-                        "Double-checked expiration dates and stored items properly\n" +
-                        "Your parent is doing well, and everything was taken care of safely and comfortably.\u2028 If you have any additional requests or follow-ups, please feel free to let me know.\n" +
-                        "I was happy to help today!",
-                isVisible = isPostAnimateVisible
-            )
+            Column(
+                modifier = Modifier
+                    .background(color = SopkathonTheme.colors.primary200)
+                    .padding(horizontal = 16.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
 
-            Spacer(modifier = Modifier.height(60.dp))
+                ProgressContentTitle(
+                    title = "Booking Info",
+                    modifier = Modifier.padding(top = 32.dp, bottom = 8.dp)
+                )
 
+                BookingInfoCard(
+                    locateText = locateText,
+                    dateText = dateText,
+                    startTime = startTime,
+                    confirmTime = confirmText,
+                    serviceFee = serviceFee,
+                    bookingFee = bookingFee,
+                    cashText = cashText,
+                    bookingInfoModel = BookingInfoModel(
+                        reportContent = bookingInfoModel.reportContent,
+                        reportBackgroundColor = SopkathonTheme.colors.primary200,
+                        reportCardHorizontalPadding = bookingInfoModel.reportCardHorizontalPadding
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                AnimateReportCard(
+                    title = "Pre-Service Report",
+                    imgRes = R.drawable.pre_img,
+                    content = "Hello, this is Sarah Johnson.\n" +
+                            "I’ll be arriving today at 11:10 PM to begin the Grocery Shopping service.\n" +
+                            "What I’ll bring:\n" +
+                            "- Cleaning supplies\n" +
+                            "- Shopping bags\n" +
+                            "- Face mask\n" +
+                            "Please relax while I take care of everything for you. I’m looking forward to helping!",
+                    isVisible = isPreAnimateVisible
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                AnimateReportCard(
+                    title = "Post-Service Report",
+                    imgRes = R.drawable.new_img,
+                    content = "Hello, this is Sarah Johnson.\n I’ve just finished the Grocery Shopping service for your parent.\n" +
+                            "Here’s what I completed today:\n" +
+                            "Purchased all items on the list\n" +
+                            "Organized everything neatly at home\n" +
+                            "Double-checked expiration dates and stored items properly\n" +
+                            "Your parent is doing well, and everything was taken care of safely and comfortably.\u2028 If you have any additional requests or follow-ups, please feel free to let me know.\n" +
+                            "I was happy to help today!",
+                    isVisible = isPostAnimateVisible
+                )
+
+                Spacer(modifier = Modifier.height(154.dp))
+            }
         }
-
-        /*SopkathonButton(
+        SopkathonButton(
             label = "Complete Request $${cashText}",
-            onClick = navigateToYubin
-        )*/
+            onClick = navigateToYubin,
+            modifier = Modifier
+                .background(color = SopkathonTheme.colors.white)
+                .align(alignment = Alignment.BottomCenter)
+                .padding(top = 12.dp, bottom = 28.dp)
+                .padding(horizontal = 16.dp),
+            isEnabled = if (isPostAnimateVisible) true else false
+        )
     }
 }
 
