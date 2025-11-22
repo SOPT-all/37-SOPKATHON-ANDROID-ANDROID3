886a935f1ca6d7f8c2cd62b0f4023195d5d78838
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2025-11-23 01:17:00 +0900

    [feat/#4] ProgressTitle background 추가

--- app/src/main/java/org/sopt/soptackthon_app_3/presentation/jimin/component/ProgressTitle.kt
+++ app/src/main/java/org/sopt/soptackthon_app_3/presentation/jimin/component/ProgressTitle.kt
@@ -1,8 +1,11 @@
 package org.sopt.soptackthon_app_3.presentation.jimin.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,6 +21,12 @@ fun ProgressTitle(
 ) {
     Column (
         modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = SopkathonTheme.colors.primary500
+            )
+            .padding(horizontal = 16.dp)
+            .padding(top = 32.dp, bottom = 50.dp)
     ){
         Text(
             text = title,
